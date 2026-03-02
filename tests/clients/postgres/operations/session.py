dd57d888e5136bf03540e4eae2b8960e7f44b9f9
Barbaron86 <kirill.molodykh@mail.ru>
2026-03-03 01:41:37 +0300

    Postgres repositories сервисов

---
+++ tests/clients/postgres/operations/session.py
@@ -0,0 +1,4 @@
+from tests.config import test_settings
+from tests.clients.postgres.session import get_postgres_test_session_factory
+
+operations_test_session_factory = get_postgres_test_session_factory(test_settings.operations_postgres_client)
