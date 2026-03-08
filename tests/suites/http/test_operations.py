e12e1371f53494704efdff47092b867a5346dc84
Barbaron86 <kirill.molodykh@mail.ru>
2026-03-08 16:50:26 +0300

    8.3 Event-driven тесты HTTP API. Практическое задание: Event-driven HTTP тесты operations-service

--- tests/suites/http/test_operations.py
+++ tests/suites/http/test_operations.py
@@ -19,7 +19,7 @@ from tests.tools.allure import AllureTag, AllureStory, AllureFeature
 class TestOperationsHTTP:
     @allure.tag(AllureTag.HTTP, AllureTag.KAFKA, AllureTag.OPERATIONS_SERVICE)
     @allure.story(AllureStory.OPERATION_EVENTS)
-    @allure.title("[HTTP][Kafka] Operation events. In progress purchase operation")
+    @allure.title("[HTTP][KAFKA] Operation events. In progress purchase operation")
     def test_operation_events_in_progress_purchase_operation(
             self,
             operations_http_test_client: OperationsHTTPTestClient,
@@ -43,7 +43,7 @@ class TestOperationsHTTP:
     )
     @allure.tag(AllureTag.HTTP, AllureTag.POSTGRES, AllureTag.OPERATIONS_SERVICE)
     @allure.story(AllureStory.OPERATION_FILTERS)
-    @allure.title("[HTTP][Postgres] Filter by card id. In progress purchase operation")
+    @allure.title("[HTTP][POSTGRES] Filter by card id. In progress purchase operation")
     def test_filter_by_card_id_in_progress_purchase_operation(
             self,
             operations_http_test_client: OperationsHTTPTestClient,
@@ -63,4 +63,35 @@ class TestOperationsHTTP:
             card_id=model.card_id
         )
 
+        assert_get_operations_response_from_models(response, [model])
+
+    @allure.tag(AllureTag.HTTP, AllureTag.KAFKA, AllureTag.OPERATIONS_SERVICE)
+    @allure.story(AllureStory.OPERATION_EVENTS)
+    @allure.title("[HTTP][KAFKA] Operation events. Completed purchase operation")
+    def test_operation_events_completed_purchase_operation(
+            self,
+            operations_http_test_client: OperationsHTTPTestClient,
+            operations_kafka_producer_test_client: OperationsKafkaProducerTestClient
+    ):
+        event = operations_kafka_producer_test_client.produce_completed_purchase_operation_event()
+        response = operations_http_test_client.get_operations(user_id=event.user_id)
+        assert_get_operations_response_from_events(response, [event])
+
+    @pytest.mark.skipif(
+        sys.platform == "win32",
+        reason="psycopg2 + Windows Unicode limitation"
+    )
+    @allure.tag(AllureTag.HTTP, AllureTag.POSTGRES, AllureTag.OPERATIONS_SERVICE)
+    @allure.story(AllureStory.OPERATION_FILTERS)
+    @allure.title("[HTTP][POSTGRES] Filter by account id. Completed purchase operation")
+    def test_filter_by_account_id_completed_purchase_operation(
+            self,
+            operations_http_test_client: OperationsHTTPTestClient,
+            operations_postgres_test_repository: OperationsPostgresTestRepository
+    ):
+        model = operations_postgres_test_repository.create_completed_purchase_operation()
+        response = operations_http_test_client.get_operations(
+            user_id=model.user_id,
+            account_id=model.account_id
+        )
         assert_get_operations_response_from_models(response, [model])
